9902daa589d1726981b64668c9d4bd08861f6b88
ALFREDO REYES LUNA <alfredo.reyes@sat.gob.mx>
2021-05-18 13:46:27 -0500

    Adding ios map

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions  } from 'react-native';
 import MapView from 'react-native-maps';
-import { Marker, Circle } from 'react-native-maps';
+import { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MapView 
         style={styles.map} 
+        provider={ PROVIDER_GOOGLE }
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
